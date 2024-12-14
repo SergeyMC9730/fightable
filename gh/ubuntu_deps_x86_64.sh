01ee0782cc70bd89cc2e82a64e3565393b6609bf
Sergei Baigerov <bdsservers@yandex.ru>
2024-12-14 21:55:31 +0800

    actions: gh script 1 fix

--- gh/ubuntu_deps_x86_64.sh
+++ gh/ubuntu_deps_x86_64.sh
@@ -2,6 +2,6 @@ echo "* installing external dependencies"
 
 sudo apt-get install libopenmpt0 libopenmpt-dev -y
 sudo apt-get install libmicrohttpd12 libmicrohttpd-dev -y
-sudo apt-get install libxrandr-dev libxinerama-dev
+sudo apt-get install libxrandr-dev libxinerama-dev libxi-dev libx11-dev libxxf86vm-dev -y
 
 echo "* done"
