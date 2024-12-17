90615601e848fb80651255a052bc9497ad26b141
Sergei Baigerov <bdsservers@yandex.ru>
2024-12-17 18:13:32 +0800

    what

--- src/tcpclient/src/destroy.c
+++ src/tcpclient/src/destroy.c
@@ -4,11 +4,14 @@
 #ifdef TARGET_UNIX
 #include <unistd.h>
 #define NPD_CLOSE close
+#define NPD_SHUTDOWN(fd, mode) shutdown(fd, mode)
 #elif defined(TARGET_WIN32)
 #include <windows.h>
 #include <winsock.h>
 #include <io.h>
 #define NPD_CLOSE _close
+#define NPD_SHUTDOWN(fd, mode)
+#define SHUT_RDWR 0
 #endif
 #include <stdio.h>
 
@@ -16,9 +19,7 @@ void _fTcpClientDestroy(struct ftcpclient *client) {
     if (!client) return;
 
     NPD_CLOSE(client->sockfd);
-#ifdef TARGET_UNIX
-    shutdown(client->sockfd, SHUT_RDWR);
-#endif
+    NPD_SHUTDOWN(client->sockfd, SHUT_RDWR);
 
     client->thread_should_exit = 1;
     pthread_cancel(client->read_thread);
