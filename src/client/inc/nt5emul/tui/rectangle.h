4cc27ca3ad7169f5bb3b4811fae280ce9eb50e32
Sergei Baigerov <bdsservers@yandex.ru>
2025-03-31 20:52:58 +0800

    import ntldr_tui from SergeyMC9730/nt5

---
+++ src/client/inc/nt5emul/tui/rectangle.h
@@ -0,0 +1,37 @@
+/*
+    nt5 -- Windows XP simulator.
+    Copyright (C) 2023  SergeyMC9730
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact SergeyMC9730 -- @dogotrigger in Discord
+*/
+
+#pragma once
+
+#include <fraylib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// draw rectangle
+void _ntTuiDrawRectangle(RLRectangle r, Color col);
+
+// draw rectangle with gradient
+void _ntTuiDrawRectangleGr(RLRectangle r, Color col_start, Color col_end);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/client/ntldr_tui/rectangle.c
@@ -0,0 +1,49 @@
+/*
+    nt5 -- Windows XP simulator.
+    Copyright (C) 2023  SergeyMC9730
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact SergeyMC9730 -- @dogotrigger in Discord
+*/
+
+#include <nt5emul/tui/environment.h>
+
+extern struct nt_tui_environment _ntTuiEnvironment;
+
+void _ntTuiDrawRectangle(RLRectangle r, Color col) {
+    DrawRectangle(
+                r.x * _ntTuiEnvironment.base_font_size.x,
+                r.y * _ntTuiEnvironment.base_font_size.y,
+
+                r.width * _ntTuiEnvironment.base_font_size.x,
+                r.height * _ntTuiEnvironment.base_font_size.y,
+
+                col
+    );
+}
+
+// draw rectangle with gradient
+void _ntTuiDrawRectangleGr(RLRectangle r, Color col_start, Color col_end) {
+    DrawRectangleGradientV(
+        r.x * _ntTuiEnvironment.base_font_size.x,
+        r.y * _ntTuiEnvironment.base_font_size.y,
+
+        r.width * _ntTuiEnvironment.base_font_size.x,
+        r.height * _ntTuiEnvironment.base_font_size.y,
+
+        col_start,
+        col_end
+    );
+}
