652870da402d3e036d63e8e831812eb5193b74fa
Sergei Baigerov <bdsservers@yandex.ru>
2025-04-01 17:45:13 +0800

    ga: add cmake

--- gh/ubuntu_deps_x86_64.sh
+++ gh/ubuntu_deps_x86_64.sh
@@ -1,6 +1,6 @@
 echo "* installing external dependencies"
 
-sudo apt-get install libopenmpt0 libopenmpt-dev libmicrohttpd12 libmicrohttpd-dev libxrandr-dev libxinerama-dev libxi-dev libx11-dev libxxf86vm-dev libxcursor-dev libxi6 libxinerama1 libxrandr2 libx11-6 libxxf86vm1 libxcursor1 libgtk-3-dev libgtk-3-0 libgl1-mesa-dev
+sudo apt-get install cmake libopenmpt0 libopenmpt-dev libmicrohttpd12 libmicrohttpd-dev libxrandr-dev libxinerama-dev libxi-dev libx11-dev libxxf86vm-dev libxcursor-dev libxi6 libxinerama1 libxrandr2 libx11-6 libxxf86vm1 libxcursor1 libgtk-3-dev libgtk-3-0 libgl1-mesa-dev
 sudo apt-get install google-android-cmdline-tools-13.0-installer make wget tar -y
 
 echo "* done"
