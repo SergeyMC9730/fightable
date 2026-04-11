e86241f586209b6e0ffa565e3c7feeb29cc215bb
Sergei Baigerov <bdsservers@yandex.ru>
2026-04-11 21:03:03 +0800

    gh: fix 17

--- src/client/main.c
+++ src/client/main.c
@@ -1,5 +1,5 @@
 
-//          Sergei Baigerov 2024 - 2025.
+//          Sergei Baigerov 2024 - 2026.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
@@ -7,6 +7,7 @@
 #include <fightable/tilemap.h>
 #include <fightable/init.h>
 #include <fightable/loader.h>
+#include <fightable/state.h>
 #include <stdio.h>
 
 struct ftilemap __tilemap;
