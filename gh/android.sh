b0754a6d488ae61291e9e9c3d1ac4fe26595f859
Sergei Baigerov <bdsservers@yandex.ru>
2026-04-17 20:50:10 +0800

    client: android fixes 3

--- gh/android.sh
+++ gh/android.sh
@@ -72,7 +72,7 @@ compile_mpt() {
 compile_mhd() {
     echo "mhd: start date: $(date)"
 
-	cd libmicrohttpd-*
+	cd libmicrohttpd-1.0.5
 
 	export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
 	export TARGET=$CLANG_TARGET
